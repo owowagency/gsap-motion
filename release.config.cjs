5c97b42102745c8a4dbecafb7bd77727e43b1698
Stef <stefvanwijchen@gmail.com>
2023-05-15 15:10:40 +0200

    build: :sparkles: add semantic release plugins

--- release.config.cjs
+++ release.config.cjs
@@ -3,8 +3,11 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
-    ["@semantic-release/npm", { pkgRoot: "dist" }],
-    ["@semantic-release/git", { assets: ["package.json", "./src/**/*.md"] }],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      { assets: ["package.json", "dist/**/*.js", "dist/**/*.cjs", "dist/**/*.d.ts"] },
+    ],
   ],
   tagFormat: "${version}",
   branches: ["main"],
