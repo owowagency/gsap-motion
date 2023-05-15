76e1af9f0bd7565b724b1fbe071e0998115272ec
Stef <stefvanwijchen@gmail.com>
2023-05-15 13:24:43 +0200

    use temp folder

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   entryPoints: ["lib/motion.ts", "lib/pointer.ts"],
   cleanOutputDir: true,
-  out: "temp_wiki",
+  out: "wiki",
   plugin: ["typedoc-plugin-markdown", "typedoc-github-wiki-theme"],
   readme: "none",
 };
