61cbf5801f359f6c4778d6ff4c956e03fe9b1415
Stef <stefvanwijchen@gmail.com>
2023-05-15 16:23:19 +0200

    refactor: :recycle: move source files to src folder

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -1,8 +1,6 @@
-const path = require("path");
-
 /** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
-  entryPoints: ["lib/motion.ts", "lib/pointer.ts"],
+  entryPoints: ["src/main.ts"],
   cleanOutputDir: true,
   out: "wiki",
   plugin: ["typedoc-plugin-markdown", "typedoc-github-wiki-theme"],
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: resolve(__dirname, "src/main.ts"),
       name: "gsapMotion",
     },
     rollupOptions: {
