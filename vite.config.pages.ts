482b85a7cc2eb6f761bbc8aff981fa79fd3576db
Stef <stefvanwijchen@gmail.com>
2023-05-17 18:13:36 +0200

    refactor(docs): :building_construction: set base url in vite config

--- vite.config.pages.ts
+++ vite.config.pages.ts
@@ -3,6 +3,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://owowagency.github.io/gsap-motion",
   build: {
     rollupOptions: {
       output: {
