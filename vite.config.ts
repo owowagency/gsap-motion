7695860cf3f00ba19804443583a665164b6d13dc
Stef <stefvanwijchen@gmail.com>
2023-05-17 14:24:31 +0200

    refactor: :recycle: change 'src' to 'lib'

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "lib/index.ts"),
       name: "gsapMotion",
     },
     rollupOptions: {
