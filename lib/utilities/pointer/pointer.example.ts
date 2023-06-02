3af7b1879c15c0fcd26775b4eea6e064b95ac019
Stef <stefvanwijchen@gmail.com>
2023-06-02 15:59:55 +0200

    refactor: :rotating_light: fix linter problems

--- lib/utilities/pointer/pointer.example.ts
+++ lib/utilities/pointer/pointer.example.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, unsafeCSS } from "lit";
-import { customElement, property, state } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import { Pointer } from "../..";
 import gsap from "gsap";
 import style from "../../../tailwind.css?inline";
