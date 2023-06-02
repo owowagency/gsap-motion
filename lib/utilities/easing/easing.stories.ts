d37919856a39750ae67d8135585c544906a42086
Stef <stefvanwijchen@gmail.com>
2023-06-02 15:55:21 +0200

    feat: :sparkles: add gsap context to motion

---
+++ lib/utilities/easing/easing.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+// More on how to set up stories at: https://storybook.js.org/docs/web-components/writing-stories/introduction
+const meta = {
+  title: "core/Easing",
+  parameters: {
+    docs: {
+      source: { code: null },
+    },
+  },
+  render() {
+    return html`<div />`;
+  },
+} satisfies Meta<any>;
+
+export default meta;
+type Story = StoryObj<any>;
+
+// More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
+export const Primary: Story = {};
--- lib/utilities/easing/easing.ts
+++ lib/utilities/easing/easing.ts
@@ -1,39 +1,79 @@
+const MAGNITUDE_BACK = 1.70158;
+const MAGNITUDE_ELASTIC = 0.7;
+
 export class Ease {
-  /** accelerating from zero velocity */
+  static inSine = (t: number) => -1 * Math.cos(t * (Math.PI / 2)) + 1;
+  static outSine = (t: number) => Math.sin(t * (Math.PI / 2));
+  static inOutSine = (t: number) => -0.5 * (Math.cos(Math.PI * t) - 1);
+
   static inQuad = (t: number) => t * t;
-
-  /** decelerating to zero velocity */
   static outQuad = (t: number) => t * (2 - t);
-
-  /** acceleration until halfway, then deceleration */
   static inOutQuad = (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t);
 
-  /** accelerating from zero velocity */
   static inCubic = (t: number) => t * t * t;
-
-  /** decelerating to zero velocity */
   static outCubic = (t: number) => --t * t * t + 1;
-
-  /** acceleration until halfway, then deceleration */
   static inOutCubic = (t: number) =>
     t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
 
-  /** accelerating from zero velocity */
   static inQuart = (t: number) => t * t * t * t;
-
-  /** decelerating to zero velocity */
   static outQuart = (t: number) => 1 - --t * t * t * t;
-
-  /** acceleration until halfway, then deceleration */
   static inOutQuart = (t: number) => (t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t);
 
-  /** accelerating from zero velocity */
   static inQuint = (t: number) => t * t * t * t * t;
-
-  /** decelerating to zero velocity */
   static outQuint = (t: number) => 1 + --t * t * t * t * t;
-
-  /** acceleration until halfway, then deceleration */
   static inOutQuint = (t: number) =>
     t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
+
+  static inExpo = (t: number) => (t === 0 ? 0 : Math.pow(2, 10 * (t - 1)));
+  static outExpo = (t: number) => (t === 1 ? 1 : -Math.pow(2, -10 * t) + 1);
+  static inOutExpo = (t: number) => {
+    if (t === 0 || t === 1) return t;
+    if (t * 2 < 1) return 0.5 * Math.pow(2, 10 * (t * 2 - 1));
+    return 0.5 * (-Math.pow(2, -10 * (t * 2 - 1)) + 2);
+  };
+
+  static inCirc = (t: number) => -1 * (Math.sqrt(1 - (t / 1) * t) - 1);
+  static outCirc = (t: number) => Math.sqrt(1 - Math.pow(t - 1, 2));
+  static inOutCirc = (t: number) => {
+    if (t * 2 < 1) return -0.5 * (Math.sqrt(1 - Math.pow(t * 2, 2)) - 1);
+    return 0.5 * (Math.sqrt(1 - Math.pow(t * 2 - 2, 2)) + 1);
+  };
+
+  static inBack = (t: number, m = MAGNITUDE_BACK) => t * t * ((m + 1) * t - m);
+  static outBack = (t: number, m = MAGNITUDE_BACK) => {
+    const s = t / 1 - 1;
+    return s * s * ((m + 1) * s + m) + 1;
+  };
+  static inOutBack = (t: number, m = MAGNITUDE_BACK) => {
+    const s = t * 2;
+    const s2 = s - 2;
+    const ms = m * 1.525;
+    if (s < 1) return 0.5 * s * s * ((ms + 1) * s - ms);
+    return 0.5 * (s2 * s2 * ((ms + 1) * s2 + ms) + 2);
+  };
+
+  static inElastic = (t: number, m = MAGNITUDE_ELASTIC) => {
+    if (t === 0 || t === 1) return t;
+    const s = t / 1;
+    const s1 = s - 1;
+    const p = 1 - m;
+    const sp = (p / (2 * Math.PI)) * Math.asin(1);
+    return -(Math.pow(2, 10 * s1) * Math.sin(((s1 - sp) * (2 * Math.PI)) / p));
+  };
+  static outElastic = (t: number, m = MAGNITUDE_ELASTIC) => {
+    if (t === 0 || t === 1) return t;
+    const p = 1 - m;
+    const s = t * 2;
+    const sp = (p / (2 * Math.PI)) * Math.asin(1);
+    return Math.pow(2, -10 * s) * Math.sin(((s - sp) * (2 * Math.PI)) / p) + 1;
+  };
+  static inOutElastic = (t: number, m = MAGNITUDE_ELASTIC) => {
+    if (t === 0 || t === 1) return t;
+    const p = 1 - m;
+    const s = t * 2;
+    const s1 = s - 1;
+    const sp = (p / (2 * Math.PI)) * Math.asin(1);
+    if (s < 1) return -0.5 * (Math.pow(2, 10 * s1) * Math.sin(((s1 - sp) * (2 * Math.PI)) / p));
+    return Math.pow(2, -10 * s1) * Math.sin(((s1 - sp) * (2 * Math.PI)) / p) * 0.5 + 1;
+  };
 }
