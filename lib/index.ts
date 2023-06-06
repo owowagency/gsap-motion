18ec04261606a2fb7e4f49f9243ffc528ec0ddcf
Stef <stefvanwijchen@gmail.com>
2023-06-06 13:56:17 +0200

    refactor: :recycle: add some comments

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,8 @@
+// utils
 export * from "./utilities/motion/motion";
 export * from "./utilities/pointer/pointer";
 export * from "./utilities/easing/easing";
 
+// presets
 export * from "./presets/marquee/marquee";
+export * from "./presets/textClipReveal/textClipReveal";
