42e4c6114a7b6539d86690977977c6ee6cc9e6b3
Stef <stefvanwijchen@gmail.com>
2023-06-06 13:58:32 +0200

    fix: :bug: fix missing module

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 // utils
+export * from "./utilities/easing/easing";
 export * from "./utilities/motion/motion";
 export * from "./utilities/pointer/pointer";
-export * from "./utilities/easing/easing";
 
 // presets
 export * from "./presets/marquee/marquee";
