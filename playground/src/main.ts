5bb95bbf07bcf7c5bc2a184056db66c57547af54
Stef <stefvanwijchen@gmail.com>
2023-09-10 21:21:58 +0200

    feat: :poop: playing around

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,33 @@
 import { gsap } from "gsap";
 import "./style.css";
-import { createMotion } from "../..";
+import { createMotion, physicsBasedMotion, createMarquee, Marquee } from "../../lib";
 
-console.log(createMotion);
+const motion = createMotion(
+  () => {
+    console.log("create dummy motion");
+    const tween = gsap.to("body", { background: "black", duration: 3, yoyo: true, repeat: -1 });
+    return (destroyed) => {
+      console.log("cleanup dummy motion", { destroyed });
+      tween.revert().kill();
+    };
+  },
+  { observeWindowResize: true }
+);
+
+const marquee = createMarquee(
+  "#marquee-2",
+  {
+    speed: 3,
+    onCreated() {
+      console.log("created");
+    },
+    onUpdate(progress) {
+      // console.log({ progress });
+    },
+  },
+  {
+    observeWindowResize: true,
+  }
+);
+
+// const marquee = new Marquee("#marquee-2");
