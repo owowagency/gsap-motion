4767c1000136a2dede1ae0a20a860d416beffaa5
Stef <stefvanwijchen@gmail.com>
2023-09-11 09:39:02 +0200

    feat: :poop: add gsap to playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,33 +1,6 @@
-import { gsap } from "gsap";
+import { SplitText } from "../gsap/all";
+import { createMarquee, createTextClipReveal } from "../../lib";
 import "./style.css";
-import { createMotion, physicsBasedMotion, createMarquee, Marquee } from "../../lib";
 
-const motion = createMotion(
-  () => {
-    console.log("create dummy motion");
-    const tween = gsap.to("body", { background: "black", duration: 3, yoyo: true, repeat: -1 });
-    return (destroyed) => {
-      console.log("cleanup dummy motion", { destroyed });
-      tween.revert().kill();
-    };
-  },
-  { observeWindowResize: true }
-);
-
-const marquee = createMarquee(
-  "#marquee-2",
-  {
-    speed: 3,
-    onCreated() {
-      console.log("created");
-    },
-    onUpdate(progress) {
-      // console.log({ progress });
-    },
-  },
-  {
-    observeWindowResize: true,
-  }
-);
-
-// const marquee = new Marquee("#marquee-2");
+const marquee = createMarquee("#marquee-2");
+const tcr = createTextClipReveal("#marquee-2", { SplitText });
