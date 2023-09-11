db57301b7456244ac0fe568b77a48cee3077539d
Stef <stefvanwijchen@gmail.com>
2023-09-11 18:26:37 +0200

    refactor: :construction_worker: update base path for nextjs static build

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,8 +3,23 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.tsx",
 });
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = withNextra({
   output: "export",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
